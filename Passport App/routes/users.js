01354938bcbcbd585484f0fd08febb805450d919
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-05-17 05:51:58 +0530

    Registeration Complete

--- Passport App/routes/users.js
+++ Passport App/routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var Router = express.Router();
+var mongojs = require('mongojs');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var stratergy = require('passport-local').Stratergy;
+var db = mongojs('passportapp', ['users']);
 
 Router.get('/login',function(req,res){
    res.render('login');
@@ -8,6 +13,54 @@ Router.get('/login',function(req,res){
 
 Router.get('/Register',function(req,res){
 	res.render('register');
+});
+
+Router.post('/Register',function(req,res){
+	console.log('registering');
+
+	var name = req.body.name;
+	var username =req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	req.checkBody('name','name field is required').notEmpty();
+
+	req.checkBody('username','username field is required').notEmpty();
+
+	req.checkBody('password','password field is required').notEmpty();
+
+	req.checkBody('password2','passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors:errors,
+			name: name,
+			username: username
+		});
+	}else{
+		var user = {
+			name: name,
+			username : username,
+			password:password
+		}
+
+		db.users.insert(user, function(err, doc){
+			if(err){
+				res.send(err);
+			}else{
+				console.log('User Added...');
+
+				//Success Message
+				req.flash('success', 'You are registered and can now log in');
+
+				// Redirect after register
+				res.location('/');
+				res.redirect('/');
+			}
+		});
+	}
 });
 
 module.exports = Router;
